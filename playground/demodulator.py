b78c5bde65073737bc0fcccbc81d72560b32b5f0
Ivica <ivica-matic@outlook.com>
2023-11-29 19:22:17 +0000

    feat: fixed filter width

--- playground/demodulator.py
+++ playground/demodulator.py
@@ -6,24 +6,29 @@ RF_CARRIER=100000
 from fir_filter_lpf_2400hz import filter_taps as fir_filter_lpf_2400hz_taps
 
 # make 1 second of noise on RF_SAMPLE_RATE with amplitude 0.1
-noise = np.random.normal(0, 0.1, int(RF_SAMPLE_RATE))
+noise = np.random.normal(0, 0.25, int(RF_SAMPLE_RATE))
 
 # make a signal 1khz above carrier
-signal = np.sin(2*np.pi*(RF_CARRIER - 1000)*np.arange(0, 1, 1/RF_SAMPLE_RATE))
+signal_wanted_1 = np.sin(2*np.pi*(RF_CARRIER + 1000)*np.arange(0, 1, 1/RF_SAMPLE_RATE))
+signal_wanted_2 = np.sin(2*np.pi*(RF_CARRIER + 1250)*np.arange(0, 1, 1/RF_SAMPLE_RATE))
+signal_unwanted_1 = np.sin(2*np.pi*(RF_CARRIER - 500)*np.arange(0, 1, 1/RF_SAMPLE_RATE))
 
-rf_signal = noise + signal
+
+
+rf_signal = sig.resample(signal_wanted_1, int(RF_SAMPLE_RATE)) + sig.resample(signal_wanted_2, int(RF_SAMPLE_RATE)) + sig.resample(signal_unwanted_1, int(RF_SAMPLE_RATE)) + sig.resample(noise, int(RF_SAMPLE_RATE))
 
 # fft plot of rf_signal
-# plt.magnitude_spectrum(rf_signal, Fs=RF_SAMPLE_RATE, scale='dB')
-# plt.title('RF Signal Spectrum')
-# plt.show()
+plt.magnitude_spectrum(rf_signal, Fs=RF_SAMPLE_RATE, scale='dB')
+plt.title('RF Signal Spectrum')
+plt.show()
 
 AF_SAMPLE_RATE=22050
 AF_LPF_LOW_CUTOFF=0
 AF_LPF_HIGH_CUTOFF=2400
 
 # lo frequency for weaver ssb demodulation is lo=carrier + 0.5*AF_LPF_CUTOFF
-rf_lo = RF_CARRIER + 0.5*(AF_LPF_LOW_CUTOFF + AF_LPF_HIGH_CUTOFF)
+# rf_lo = RF_CARRIER + 0.5*(AF_LPF_LOW_CUTOFF + AF_LPF_HIGH_CUTOFF)
+rf_lo = RF_CARRIER + AF_LPF_HIGH_CUTOFF
 print(f"RF LO frequency for weaver ssb demodulation is {rf_lo} Hz")
 
 # make 1 second of rf_lo signal for i and q
@@ -43,11 +48,11 @@ q_rf_signal_at_rf_sample_rate = rf_signal * q_rf_lo
 i_af_signal = sig.resample(i_rf_signal_at_rf_sample_rate, int(AF_SAMPLE_RATE))
 q_af_signal = sig.resample(q_rf_signal_at_rf_sample_rate, int(AF_SAMPLE_RATE))
 
-plt.plot(np.arange(0,1,1/AF_SAMPLE_RATE), i_af_signal, label='I')
-plt.plot(np.arange(0,1,1/AF_SAMPLE_RATE), q_af_signal, label='Q')
-plt.legend()
-plt.title('I and Q AF Signals before filtering - after first mixer')
-plt.show()
+# plt.plot(np.arange(0,1,1/AF_SAMPLE_RATE), i_af_signal, label='I')
+# plt.plot(np.arange(0,1,1/AF_SAMPLE_RATE), q_af_signal, label='Q')
+# plt.legend()
+# plt.title('I and Q AF Signals before filtering - after first mixer')
+# plt.show()
 
 
 # fft plot of i_rf_signal
@@ -64,7 +69,8 @@ q_af_signal_filtered = sig.lfilter(fir_filter_lpf_2400hz_taps, 1.0, q_af_signal)
 # plt.title('I AF Signal Filtered Spectrum')
 # plt.show()
 
-af_lo = 0.5*(AF_LPF_LOW_CUTOFF + AF_LPF_HIGH_CUTOFF)
+# af_lo = 0.5*(AF_LPF_LOW_CUTOFF + AF_LPF_HIGH_CUTOFF)
+af_lo = AF_LPF_HIGH_CUTOFF
 print(f"AF LO frequency for weaver ssb demodulation is {af_lo} Hz")
 
 # make 1 second of af_lo signal for i and q
@@ -76,11 +82,11 @@ i_af_signal_filtered_at_af_sample_rate = i_af_signal_filtered * i_af_lo
 q_af_signal_filtered_at_af_sample_rate = q_af_signal_filtered * q_af_lo
 
 # plot i and q af signals
-plt.plot(np.arange(0,1,1/AF_SAMPLE_RATE), i_af_signal_filtered_at_af_sample_rate, label='I')
-plt.plot(np.arange(0,1,1/AF_SAMPLE_RATE), q_af_signal_filtered_at_af_sample_rate, label='Q')
-plt.legend()
-plt.title('I and Q AF Signals after filtering - after second mixer')
-plt.show()
+# plt.plot(np.arange(0,1,1/AF_SAMPLE_RATE), i_af_signal_filtered_at_af_sample_rate, label='I')
+# plt.plot(np.arange(0,1,1/AF_SAMPLE_RATE), q_af_signal_filtered_at_af_sample_rate, label='Q')
+# plt.legend()
+# plt.title('I and Q AF Signals after filtering - after second mixer')
+# plt.show()
 
 # add i_af_signal_filtered_at_af_sample_rate and q_af_signal_filtered_at_af_sample_rate
 af_signal = i_af_signal_filtered_at_af_sample_rate + q_af_signal_filtered_at_af_sample_rate
